c3104f7eaabef941d101880c35dfa228a6c015c1
Sócrates Díaz <sdsxd1800@gmail.com>
2025-08-03 07:21:24 -0400

    feat: add support for web exports

--- flake.nix
+++ flake.nix
@@ -3,20 +3,24 @@
     naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
+  outputs = { self, nixpkgs, utils, naersk, rust-overlay }:
     utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
         naersk-lib = pkgs.callPackage naersk { };
       in
       {
         defaultPackage = naersk-lib.buildPackage ./.;
         devShell = with pkgs; mkShell {
           buildInputs = [
-            cargo
-            rustc
+            (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+              extensions = [ "rust-src" ];
+              targets = [ "wasm32-unknown-emscripten" ];
+            }))
             rustfmt
             pre-commit
             rustPackages.clippy
@@ -24,8 +28,15 @@
             tmux
             watchexec
             lazygit
+            eza
+            fd
+            godot
           ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
+
+          shellHook = ''
+              echo "🦀 Dodge the Rust! 🦀"
+            '';
         };
       }
     );
